1df2ec7e3c8710d9765346a2df837b8d69d7367c
shardik95 <shardik95@ccs.neu.edu>
2018-07-28 18:39:16 -0400

    Questions rendered with score and progressbar

--- Quizzler/QuestionBank.swift
+++ Quizzler/QuestionBank.swift
@@ -10,6 +10,37 @@ import Foundation
 
 class QuestionBank{
     
+    var list = [Question]();
     
+    init(){
+        
+        let item = Question(givenQuestion: "Valentine\'s day is banned in Saudi Arabia.", givenAns: true)
+        list.append(item)
+
+        list.append(Question(givenQuestion: "A slug\'s blood is green.", givenAns: true))
+        
+        list.append(Question(givenQuestion: "Approximately one quarter of human bones are in the feet.", givenAns: true))
+        
+        list.append(Question(givenQuestion: "The total surface area of two human lungs is approximately 70 square metres.", givenAns: true))
+        
+        list.append(Question(givenQuestion: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", givenAns: true))
+        
+        list.append(Question(givenQuestion: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", givenAns: false))
+        
+        list.append(Question(givenQuestion: "It is illegal to pee in the Ocean in Portugal.", givenAns: true))
+        
+        list.append(Question(givenQuestion: "You can lead a cow down stairs but not up stairs.", givenAns: false))
+        
+        list.append(Question(givenQuestion: "Google was originally called \"Backrub\".", givenAns: true))
+        
+        list.append(Question(givenQuestion: "Buzz Aldrin\'s mother\'s maiden name was \"Moon\".", givenAns: true))
+        
+        list.append(Question(givenQuestion: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", givenAns: false))
+        
+        list.append(Question(givenQuestion: "No piece of square dry paper can be folded in half more than 7 times.", givenAns: false))
+        
+        list.append(Question(givenQuestion: "Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.", givenAns: true))
+        
+    }
     
 }
--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -4,6 +4,10 @@ class ViewController: UIViewController {
     
     //Place your instance variables here
     
+    let allQuestions  = QuestionBank();
+    var pressed : Bool = true;
+    var questionNumber : Int = 0;
+    var score: Int = 0;
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -12,32 +16,62 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        nextQuestion();
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        if(sender.tag==1){
+            pressed = true;
+        }
+        else{
+            pressed = false;
+        }
+        checkAnswer();
+        questionNumber = questionNumber + 1;
+        nextQuestion();
     }
     
     
     func updateUI() {
-      
+        scoreLabel.text = "Score: \(score)";
+        progressLabel.text = "\(questionNumber+1)/13";
+        progressBar.frame.size.width = (view.frame.size.width/13) * CGFloat (questionNumber + 1);
     }
     
 
     func nextQuestion() {
-        
+        if(questionNumber <= 12){
+            questionLabel.text = allQuestions.list[questionNumber].question
+            
+            updateUI();
+        }
+        else{
+            let alert = UIAlertController(title: "Quiz Complete", message: "Restart the Quiz", preferredStyle: .alert);
+            
+            alert.addAction(UIAlertAction(title: "Restart", style: .default, handler: {(alert: UIAlertAction!) in self.startOver()}));
+            
+            self.present(alert, animated: true, completion: nil);
+        }
     }
     
     
     func checkAnswer() {
-        
+        let correctAns = allQuestions.list[questionNumber].answer;
+        if(correctAns == pressed){
+            print("Correct");
+            score += 1;
+        }
+        else{
+            print("Incorrect");
+        }
     }
     
     
     func startOver() {
-       
+        questionNumber = 0;
+        score = 0;
+        nextQuestion();
     }
     
 
